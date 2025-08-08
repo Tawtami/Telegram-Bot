6a8e1e30dc54b2479b3bb62248322bdd6f270585
Taha Hatami <tahahatami1998@gmail.com>
2025-08-08 11:53:10 +0330

    fix: remove router import from handlers/__init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Bot handlers module
+Bot handlers package
 """
 
-from .registration import router as registration_router
+from .registration import build_registration_conversation
+from .menu import send_main_menu, handle_menu_selection, handle_back_to_menu
+from .courses import (
+    handle_free_courses,
+    handle_paid_courses,
+    handle_purchased_courses,
+    handle_course_registration,
+    handle_payment_receipt,
+)
+from .books import build_book_purchase_conversation
+from .social import handle_social_media
+from .contact import handle_contact_us
 
 __all__ = [
-    "registration_router",
+    "build_registration_conversation",
+    "send_main_menu",
+    "handle_menu_selection",
+    "handle_back_to_menu",
+    "handle_free_courses",
+    "handle_paid_courses",
+    "handle_purchased_courses",
+    "handle_course_registration",
+    "handle_payment_receipt",
+    "build_book_purchase_conversation",
+    "handle_social_media",
+    "handle_contact_us",
 ]
